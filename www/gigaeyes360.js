27a9cc953dd6cd3273c8dec904445a936f9319fc
civan <civan.blaze@gmail.com>
2017-06-26 15:15:14 +0900

    modify

--- www/gigaeyes360.js
+++ www/gigaeyes360.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.watchPanorama = function(moviePath, success, error) {
-    exec(success, error, "gigaeyes360", "watchPanorama", [moviePath]);
+    exec(success, error, "Gigaeyes360", "watchPanorama", [moviePath]);
 };
 
 exports.watch = function(moviePath, user, password, success, error) {
-	exec(success, error, "gigaeyes360", "watch", [moviePath, user, password]);
+	exec(success, error, "Gigaeyes360", "watch", [moviePath, user, password]);
 };
