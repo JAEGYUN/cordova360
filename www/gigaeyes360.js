cb171cf7072b92a3d377e0c778f4369035d60673
civan <civan.blaze@gmail.com>
2017-07-19 12:37:49 +0900

    update

--- www/gigaeyes360.js
+++ www/gigaeyes360.js
@@ -4,6 +4,10 @@ exports.watchPanorama = function(moviePath, success, error) {
     exec(success, error, "Gigaeyes360", "watchPanorama", [moviePath]);
 };
 
+exports.watchPanorama = function(moviePath, title, success, error) {
+    exec(success, error, "Gigaeyes360", "watchPanorama", [moviePath, title]);
+};
+
 exports.watch = function(moviePath, user, password, success, error) {
 	exec(success, error, "Gigaeyes360", "watch", [moviePath, user, password]);
 };
